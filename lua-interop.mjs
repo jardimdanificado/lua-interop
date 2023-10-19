7dc53b75ba3fcddf713f3e824ad172a9613267d6
jardimdanificado <jardeltorreslima01@gmail.com>
2023-10-19 02:55:20 -0300

    added a ai general description about the project

--- lua-interop.mjs
+++ lua-interop.mjs
@@ -1,3 +1,49 @@
+/*
+    DESCRIÇÃO GERADA POR INTELIGENCIA ARTIFICIAL!
+    DESCRIÇÃO GERADA POR INTELIGENCIA ARTIFICIAL!
+    DESCRIÇÃO GERADA POR INTELIGENCIA ARTIFICIAL!
+
+    esse módulo não necessita de dependencias externas
+    esse arquivo é um módulo javascript que permite a comunicação com o lua através de um child_process do nodejs e do protocolo de comunicação definido no arquivo lua-interop.lua
+    para usar esse módulo, basta inicializar os git submodules(luatils), importar o arquivo lua-interop.lua no seu script lua e importar esse módulo no seu script javascript
+    o módulo exporta duas classes: LuaSession e LuaSessionManager
+    o módulo é feito para ser usado asincronamente, então todos os métodos retornam uma promise
+    LuaSession é uma classe que representa uma sessão lua, ela é responsável por enviar comandos para o lua e receber respostas
+    LuaSessionManager é uma classe que representa um gerenciador de sessões lua, ela é responsável por gerenciar várias sessões lua e distribuir os comandos entre elas
+    LuaSessionManager é recomendado para aplicações que precisam de muitas sessões lua, como por exemplo, um servidor de um jogo multiplayer
+    LuaSession é recomendado para aplicações que precisam de poucas sessões lua, como por exemplo, um bot de discord
+    LuaSessionManager possui um método require que permite carregar bibliotecas lua em todas as sessões
+    LuaSessionManager possui um método close que fecha todas as sessões
+    LuaSession possui um método close que fecha a sessão
+    LuaSessionManager e LuaSession possuem os métodos json, text e log que permitem enviar dados para o lua
+    LuaSessionManager e LuaSession possuem o método eval que permite enviar comandos lua para o lua e receber respostas
+    o módulo tambem exporta tolua e fromlua, que são funções que permitem converter dados entre lua e javascript
+    tolua converte dados javascript para lua
+    fromlua converte dados lua para javascript
+    para usar essas funções, basta usar elas como se fossem uma função lua
+    exemplo:
+    const { tolua, fromlua } = require('./lua-interop.mjs');
+    const data = { a: 1, b: 2 };
+    const luaData = tolua(data);
+    console.log(luaData); // { a: 1, b: 2 }
+    const jsData = fromlua(luaData);
+    console.log(jsData); // { a: 1, b: 2 }
+    para usar o módulo, basta importar ele no seu script javascript
+    exemplo:
+    const { LuaSession, LuaSessionManager } = require('./lua-interop.mjs');
+    const session = new LuaSession();
+    session.eval('print("Hello World!")');
+    session.close();
+
+    USE O MÓDULO COM SABEDORIA!
+    USE O ARQUIVO setup.sh PARA INSTALAR AS DEPENDENCIAS LUA!
+    DEPENDENCIAS DO LUA: luatils
+
+    DESCRIÇÃO GERADA POR INTELIGENCIA ARTIFICIAL!
+    DESCRIÇÃO GERADA POR INTELIGENCIA ARTIFICIAL!
+    DESCRIÇÃO GERADA POR INTELIGENCIA ARTIFICIAL!
+*/
+
 import { spawn } from 'child_process';
 
 export function tolua(data) 
