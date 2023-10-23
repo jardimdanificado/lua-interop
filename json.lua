212cc5b8659b864a607074e3bd397fd4f22ae919
jardimdanificado <jardeltorreslima01@gmail.com>
2023-10-22 23:56:59 -0300

    0.1.61 fixed a string that could be misundertood

--- json.lua
+++ json.lua
@@ -22,7 +22,7 @@
 -- SOFTWARE.
 --
 
--- fork da versão oficial 0.1.2 por rxi
+-- fork da versão oficial 0.1.2
 local JSON = { _version = "0.1.3" }
 
 -------------------------------------------------------------------------------
