3f7e1a617e30d035559c82342d2d43d82fe5f841
jardimdanificado <jardeltorreslima01@gmail.com>
2023-10-27 07:09:56 -0300

    0.2.2: .direct(): a faster  alternative to .eval()

--- examples/cexample.mjs
+++ examples/cexample.mjs
@@ -4,7 +4,7 @@ const s = new PipeSession('./repl.exe');
 const lua = new PipeSession('luajit', ['init.lua']);
 let txt = await lua.eval('text("Hello World!")');
 process.stdout.write(txt)
-txt = await s.eval('print("Hello World!")')
+txt = await s.direct('print("Hello World!")')
 process.stdout.write(txt)
 s.close();
 lua.close();
--- potpiper.mjs
+++ potpiper.mjs
@@ -204,6 +204,74 @@ export class PipeSession
         return this.task;
     }
 
+    async direct(command, timeout) //same as eval but doesn't parse the output so its faster
+    {
+        timeout = timeout ?? this.default_timeout ?? null;
+        if (this.busy) 
+        {
+            if (timeout) 
+            {
+                return new Promise((resolve, reject) => 
+                {
+                    this.queue.push({ command, timeout, resolve, reject});
+                });
+            }
+            else 
+            {
+                return new Promise((resolve, reject) => 
+                {
+                    this.queue.push({ command, undefined, resolve, reject });
+                });
+            }
+        }
+
+        this.busy = true;
+        this.task = new Promise((resolve, reject) => 
+        {
+            this.childprocess.stdin.write(command + '\n');
+            const onDataHandler = (data) => 
+            {
+                let str = data.toString();
+                this.childprocess.stdout.off('data', onDataHandler); // Remove o manipulador de eventos
+                try
+                {
+                    //str = str.substring(1, str.length - 1);
+                    this.busy = false;
+                    this.task = null;
+                    if (this.timeout) 
+                    {
+                        clearTimeout(this.timeout);
+                        this.timeout = null;
+                    }
+                    resolve(str);
+                    advanceQueue(this);
+                }
+                catch (error)
+                {
+                    this.busy = null;
+                    this.task = null;
+                    if (this.timeout) 
+                    {
+                        clearTimeout(this.timeout);
+                        this.timeout = null;
+                    }
+                    reject(error);
+                    advanceQueue(this);
+                }
+
+                if(timeout)
+                {
+                    this.timeout = setTimeout(() =>
+                    {
+                        this.childprocess.stdout.off('data', onDataHandler); // Remove o manipulador de eventos
+                        resolve('Timed out');
+                    }, timeout);
+                }
+            };
+            this.childprocess.stdout.on('data', onDataHandler);
+        });
+        return this.task;
+    }
 
     json = async (data,timeout) => 
     {
